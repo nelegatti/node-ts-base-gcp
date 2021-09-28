bf1b7d875260f7b1edaa5801d2d9fb23ca998604
nelegatti <nelegatti.93@gmail.com>
2021-09-28 09:09:10 -0300

    Include little readme

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
+// The Firebase Admin SDK to access Firestore.
 admin.initializeApp();
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -8,6 +9,8 @@ admin.initializeApp();
 enum collectionsEnum {
   messages = "messages"
 }
+
+const db = admin.firestore();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {
@@ -20,7 +23,7 @@ export const addMessage = functions.https.onRequest(async (req, res) => {
   const original = req.query.text;
 
   const writeResult =
-    await admin.firestore().collection(collectionsEnum.messages).add({
+    await db.collection(collectionsEnum.messages).add({
       original: original,
     });
 
@@ -29,11 +32,25 @@ export const addMessage = functions.https.onRequest(async (req, res) => {
   });
 });
 
-export const makeUppercase = functions.firestore.document("")
-    .onCreate((snap, context) => {
-      const original = snap.data().original;
-      functions.logger.log("Uppercasing", context.params.documentId, original);
-      const upperCase = original.toUpperCase();
+export const makeUppercase =
+  functions.firestore.document(`/${collectionsEnum.messages}/{documentId}`)
+      .onCreate((snap, context) => {
+        const original = snap.data().original;
+        functions.logger.log("Uppercasing", context.params.documentId, original);
+        const upperCase = original.toUpperCase();
 
-      return snap.ref.set({upperCase}, {merge: true});
-    });
+        return snap.ref.set({upperCase}, {merge: true});
+      });
+
+export const getMessages = functions.https.onRequest(async (req, res) => {
+  const result = await db.collection(collectionsEnum.messages).get();
+  res.json(result);
+});
+
+export const updateMessage = functions.https.onRequest(async (req, res) => {
+  const messageId = req.query.id as string;
+  const result = await db.collection(collectionsEnum.messages).doc(messageId).set({
+    original: "valueUpdated",
+  });
+  res.json(result);
+});
